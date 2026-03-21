1de330e120c0244ba9f4c6f5207035ab8401fe88
Alexander Yunashev <yunashevy@mail.ru>
2026-03-22 00:06:24 +0700

    Описано состояние EmptyState как текст и картинка вложенные в Column и Box

--- app/src/main/java/com/example/description_county/ui/CountryScreen.kt
+++ app/src/main/java/com/example/description_county/ui/CountryScreen.kt
@@ -1,7 +1,10 @@
 package com.example.description_county.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -11,13 +14,19 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.description_county.R
+
+import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.*
 
 @Composable
 fun TestScreen() {
-//    var text by remember { mutableStateOf("") }
     var state by remember { mutableStateOf<SearchState>(SearchState.Empty()) }
-    Column() {
+    Column(modifier = Modifier.fillMaxSize()) {
         Row {
             EditText(
                 text = state.query,
@@ -48,7 +57,20 @@ fun EditText(text: String, onTextChange: (String) -> Unit, modifier: Modifier = 
 
 @Composable
 fun EmptyState(){
-    Text("Введите страну")
+    Box(modifier = Modifier.fillMaxSize().padding(24.dp),
+        contentAlignment = Alignment.Center)
+    {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.outline_feature_search_24),
+                contentDescription = null,
+                modifier = Modifier.size(96.dp)
+            )
+            Text("Введите страну")
+        }
+    }
 }
 
 @Composable
